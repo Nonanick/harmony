1f653ffda4f410afc38958c7dc1d7aec85259360
Nicholas Frai <nicfrai@gmail.com>
2021-03-27 00:50:25 -0300

    Update package json

--- projects/webapp/src/App.svelte
+++ projects/webapp/src/App.svelte
@@ -1,11 +1,6 @@
 <script lang="ts">
-<<<<<<< HEAD
-import LineSeparator from './components/interface/line_separator/LineSeparator.svelte';
-
-=======
 	import RouterViewport from "./components/router/RouterViewport.svelte";
 	import ThemeApplier from "./themes/ThemeApplier.svelte";
->>>>>>> 658d8da47a9f077fbf9d4004419b63d1d8fdb55f
 </script>
 
 <ThemeApplier>
